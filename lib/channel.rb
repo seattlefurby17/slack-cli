c06a7ce2632a0b8513e4ecb36143148680e3bbe1
Beatrice T <be@trice.codes>
2020-10-06 16:17:29 -0700

    Finished class scaffolding

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,21 +1,37 @@
 
 class Channel < Recipient
-  attr_reader :slack_id, :name
+  attr_reader :slack_id, :name, :topic, :member_count
 
-  def initialize(slack_id, name)
+  def initialize(slack_id, name, topic, member_count)
     super(slack_id, name)
+    @topic = topic
+    @member_count = member_count
   end
 
   def details
-
+    puts "Slack ID: #{slack_id}"
+    puts "Name: #{name}"
+    puts "Topic: #{topic}"
+    puts "Member Count: #{member_count}"
   end
 
   def self.list_all
     # response['channels'].each do |channel|
     #   puts channel['name']
-    end
-
+    # return [channel1, channel2, channel3, channel4]
   end
 
+  # @channels = Channel.list_all # returns array of channels # Class method
+  # @channels.each do |channel|
+  #   puts channel.details # Instance method
+  # end
+
+  def details
+    puts @slack_id
+    puts @name
+    puts @topic
+    puts @member_count
+  end
 
 end
+
