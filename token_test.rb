d226388c0e6c6edcc894deaaef7036ebc2d3a341
Hanh Solo <minhhanhnyc@gmail.com>
2020-10-06 13:48:34 -0700

    Files updated

--- token_test.rb
+++ token_test.rb
@@ -8,16 +8,16 @@ Dotenv.load
 
 def main
   puts 'Welcome to the Ada Slack CLI!'
-  puts ENV['SLACK_API_TOKEN']
+  puts ENV['SLACK_TOKEN']
 
-  # response = HTTParty.get('https://api.slack.com/methods/conversations.list', query: {
-  #                           token: ENV['SLACK_API_TOKEN']
-  #                         })
-  response = HTTParty.post('https://api.slack.com/methods/conversations.list', body: {
-                             token: ENV['SLACK_API_TOKEN'],
-                             channel: 'test-channel2', # this is our test channel, we need to create one.
-                             text: 'This is my test~!'
-                           })
+  response = HTTParty.get('https://slack.com/api/users.list', query: {
+                            token: ENV['SLACK_TOKEN']
+                          })
+  # response = HTTParty.post('https://api.slack.com/methods/conversations.list', body: {
+  #                            token: ENV['SLACK_API_TOKEN'],
+  #                            channel: 'test-channel2', # this is our test channel, we need to create one.
+  #                            text: 'This is my test~!'
+  #                          })
 
   pp response
   puts 'Thank you for using the Ada Slack CLI'
