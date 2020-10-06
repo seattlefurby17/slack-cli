a163d399f6880874180db0e4b6026ae685bc8663
Beatrice T <be@trice.codes>
2020-10-06 11:41:18 -0700

    Set up environment to hide secrets

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,10 +11,10 @@ require 'vcr'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-VCR.configure do |config|
-  config.cassette_library_dir = "test/cassettes"
-  config.hook_into :webmock
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "test/cassettes"
+#   config.hook_into :webmock
+# end
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
@@ -25,5 +25,7 @@ VCR.configure do |config|
   }
 
   # Don't leave our token lying around in a cassette file.
-
+  config.filter_sensitive_data("<SLACK_TOKEN>") do
+    ENV["SLACK_TOKEN"]
+  end
 end
