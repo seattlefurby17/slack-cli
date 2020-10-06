687b1de98db1e7e1fc275efc38951a7cb779e452
Beatrice T <be@trice.codes>
2020-10-06 14:07:36 -0700

    Finished token_test API verification

--- token_test.rb
+++ token_test.rb
@@ -7,7 +7,6 @@ Dotenv.load
 
 def main
   puts 'Welcome to the Ada Slack CLI!'
-  puts ENV['SLACK_TOKEN']
 
   response = HTTParty.get('https://slack.com/api/conversations.list', query: {
                              token: ENV['SLACK_TOKEN']
@@ -18,9 +17,15 @@ def main
   #                           text: 'This is my test~!'
   #                         })
 
-  pp response
-  pp response.code
-  pp response.message
+
+  if response.code != 200 || response.message != "OK"
+    puts "There was a problem!"
+    puts "The code was #{response.code} and the message was #{response.message}"
+  end
+
+  response['channels'].each do |channel|
+    puts channel['name']
+  end
 
   puts 'Thank you for using the Ada Slack CLI'
 end
