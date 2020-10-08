660007a87a56bc148955c50eb29a71d9ff2dbe87
Hanh Solo <minhhanhnyc@gmail.com>
2020-10-07 17:15:38 -0700

    First draft of driver code in slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,16 +1,33 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require_relative 'workspace'
 require 'table_print'
 
+def get_command
+  puts 'What would you like to do? '
+  puts "1 = list users\n2 = list channels \n3 = quit/q"
+  command = gets.chomp
+  command
+end
+
 def main
-  puts "Welcome to the Ada Slack CLI!"
+  puts 'Welcome to the Ada Slack CLI!'
   workspace = Workspace.new
 
-  tp workspace.channels, 'slack_id', 'name', 'member_count', 'topic'
-
-  tp workspace.users, 'slack_id', 'name', 'real_name', 'status_text', 'status_emoji'
-
-  puts "Thank you for using the Ada Slack CLI"
+  command = get_command
+  while command != '3' && command != "quit" && command != 'q'
+    case command
+    when '1', 'list users'
+      tp workspace.users, 'slack_id', 'name', 'real_name'
+    when '2', 'list channels'
+      tp workspace.channels, 'name', 'topic', 'member_count', 'slack_id'
+    else
+      puts 'Please only select the commands from the list'
+    end
+    command = get_command
+  end
+  puts 'Thank you for using the Ada Slack CLI'
 end
 
 main if __FILE__ == $PROGRAM_NAME
